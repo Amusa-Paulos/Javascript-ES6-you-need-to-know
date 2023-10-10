e00105506a6bd9d8b87651116f6209dbdceace75
Abayomi Amusa <amusaabayomipaul99@gmail.com>
2023-10-10 22:58:30 +0100

    Lesson files of Let & Const

---
+++ index.js
@@ -0,0 +1,35 @@
+// Let & Const
+
+// Let docs
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
+
+// var name = "whatever"
+
+// let friends = ['t']
+// friends = "drake"
+
+// {
+//     let friends = 90
+//     console.log(friends);
+//     friends = 34
+// }
+
+// let friends = "music"
+
+// var name = 78
+
+// console.log(friends);
+
+// Const
+// Read more on Const 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
+const whatIsName = "myname"
+
+{
+    // whatIsName = 78
+    console.log("b:", whatIsName);
+}
+
+// const whatIsName = 90
+
+console.log(whatIsName);
