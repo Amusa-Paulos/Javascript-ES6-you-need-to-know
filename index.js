82c4f9c4c2fa3bcf5134e4f5e1d69543778fd817
Abayomi Amusa <amusaabayomipaul99@gmail.com>
2023-10-10 23:15:04 +0100

    Lesson files of For...of

---
+++ index.js
@@ -0,0 +1,14 @@
+// For...of
+
+const fakeData = {name: "john", age: 90, lastName: "doe"}
+
+const fakeArr = ["john", 'doe', 78]
+
+// Arrays, Set, NodeList 
+
+// fakeArr.map()
+// fakeArr.forEach()
+
+for (const value of fakeArr) {
+    console.log(value)
+}
